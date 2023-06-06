74d746ed88bc0a3e3e51ee0ef76e568e8e108faa
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2023-06-06 15:17:37 -0300

    Update index.jsx

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -29,7 +29,7 @@ export default memo(function Sidebar({ children }) {
     return (
         <div className="flex w-screen">
             <div className="relative select-none bg-primary">
-                <div className="w-80 text-sm h-screen overflow-y-scroll overflow-x-hidden scroll-2 bg-shadow dark:bg-gray flex flex-col">
+                <div className="w-80 text-sm h-screen overflow-y-scroll overflow-x-hidden scroll-2 bg-shadow flex flex-col">
                     <div className="py-6">
                         <img src="/img/logo.png" alt="Logo Contato Seguro - Canal de Ética" className="max-w-[190px] w-full m-auto" />
                     </div>
@@ -53,7 +53,7 @@ export default memo(function Sidebar({ children }) {
                     </div>
                 </div>
             </div>
-            <div className="scroll-1 h-screen overflow-y-scroll duration-300 bg-gray-50 dark:bg-black w-full p-12">
+            <div className="scroll-1 h-screen overflow-y-scroll duration-300 bg-gray-50 w-full p-12">
                 <h1 className="mb-10 font-bold text-4xl">{activeMenuName}</h1>
                 <main className="bg-primary p-5 rounded-xl">{children}</main>
             </div>
